7ddcb4353c6d8adbb95cf59dc22dede8889300f0
David Torres <davidtorres@microsoft.com>
2023-08-18 04:06:58 -0700

    Updates

--- Week6/sql/institution_ic_academic_charges.5.add FK.sql
+++
@@ -1,4 +0,0 @@
-
-        ALTER TABLE institution_ic_academic_charges 
-        ADD CONSTRAINT fk_institution_ic_academic_charges_to_institution
-            FOREIGN KEY (inst_id) REFERENCES institution(inst_id);
